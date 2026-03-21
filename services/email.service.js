3d3cd8208b2094ee51050027dc8e09e2bf0e3574
Ojeyinka Ifeoluwa Samuel <samuelojeyinka@gmail.com>
2026-03-21 21:09:05 +0100

    ..

--- services/email.service.js
+++ services/email.service.js
@@ -3,14 +3,11 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
-  secure: true, // use SSL
+  service: 'gmail',
+  // secure: true, // use SSL
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
-  },
-  tls: {
-    // do not fail on invalid certs
-    rejectUnauthorized: false
   }
 });
 
